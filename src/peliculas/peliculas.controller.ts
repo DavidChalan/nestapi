fd652516551a9a931a10e2c7d943d482d8310fdc
David <davidchalan01@gmail.com>
2025-02-02 12:36:03 +0100

    fin de creacion de actividad de peliculas

--- src/peliculas/peliculas.controller.ts
+++ src/peliculas/peliculas.controller.ts
@@ -3,32 +3,40 @@ import {
   Get,
   Post,
   Body,
-  // Patch,
-  // Param,
-  // Delete,
-  // Put,
+  Put,
   Query,
   Param,
   Delete,
 } from '@nestjs/common';
 import { PeliculasService } from './peliculas.service';
 import { CreatePeliculaDto } from './dto/create-pelicula.dto';
-// import { UpdatePeliculaDto } from './dto/update-pelicula.dto';
-// import { query } from 'express';
-// import { title } from 'process';
 import { Pelicula } from './entities/pelicula.entity';
+import { UpdatePeliculaDto } from './dto/update-pelicula.dto';
 
 @Controller('peliculas')
 export class PeliculasController {
   constructor(private readonly peliculasService: PeliculasService) {}
 
   @Post()
-  create(@Body() createPeliculaDto: CreatePeliculaDto) {
+  async create(
+    @Body() createPeliculaDto: CreatePeliculaDto,
+  ): Promise<Pelicula> {
     return this.peliculasService.create(createPeliculaDto);
   }
-
   @Get()
-  findByCriterial(
+  //Pelicula[] --> se utilza esto cuando hay un listado []
+  async findAll(): Promise<Pelicula[]> {
+    return this.peliculasService.findAll();
+  }
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Pelicula> {
+    return this.peliculasService.findOne(+id);
+  }
+  //buscar por titulo o año
+  // ejemplo http://localhost:5000/peliculas?year=2025
+  // http://localhost:5000/peliculas?title=david
+  @Get('Search')
+  async findByCriterial(
     @Query('title') title: string,
     @Query('year') year: number,
   ): Promise<Pelicula[]> {
@@ -39,25 +47,15 @@ export class PeliculasController {
     }
     return Promise.resolve([]);
   }
-
-  @Get()
-  async findAll(): Promise<Pelicula[]> {
-    return this.peliculasService.findAll();
+  //actualizar datos
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updatePeliculaDto: UpdatePeliculaDto,
+  ): Promise<string> {
+    return this.peliculasService.update(+id, updatePeliculaDto);
   }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.peliculasService.findOne(+id);
-  // }
-
-  // @Put(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updatePeliculaDto: UpdatePeliculaDto,
-  // ) {
-  //   return this.peliculasService.update(+id, updatePeliculaDto);
-  // }
-
+  //borrar datos
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<{ message: string }> {
     await this.peliculasService.remove(+id);
--- src/peliculas/peliculas.service.ts
+++ src/peliculas/peliculas.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePeliculaDto } from './dto/create-pelicula.dto';
 import { Pelicula } from './entities/pelicula.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LessThan, Repository } from 'typeorm';
+import { UpdatePeliculaDto } from './dto/update-pelicula.dto';
 
 @Injectable()
 export class PeliculasService {
@@ -10,13 +11,22 @@ export class PeliculasService {
     @InjectRepository(Pelicula, 'base3')
     private peliculaRepository: Repository<Pelicula>,
   ) {}
-  create(CreatePeliculaDto: CreatePeliculaDto) {
+  //crear una pelicula
+  async create(CreatePeliculaDto: CreatePeliculaDto): Promise<Pelicula> {
     const pelicula = this.peliculaRepository.create(CreatePeliculaDto);
     return this.peliculaRepository.save(pelicula);
   }
   //Obtener todas las películas
   async findAll(): Promise<Pelicula[]> {
     return this.peliculaRepository.find();
+  }
+  //Buscar peliculas por id
+  async findOne(id: number): Promise<Pelicula> {
+    const pelicula = await this.peliculaRepository.findOne({ where: { id } });
+    if (!pelicula) {
+      throw new NotFoundException(`Película con id ${id} no encontrada`);
+    }
+    return pelicula;
   }
   //Buscar películas por titulo:
   findTitle(title: string): Promise<Pelicula[]> {
@@ -26,12 +36,19 @@ export class PeliculasService {
   findByYearLessThan(year: number): Promise<Pelicula[]> {
     return this.peliculaRepository.find({ where: { año: LessThan(year) } });
   }
-  //Crear una nueva película:
-  createfile(pelicula: Pelicula): Promise<Pelicula> {
-    return this.peliculaRepository.save(pelicula);
+  //actualizar  pelicula
+  async update(
+    id: number,
+    UpdatePeliculaDto: UpdatePeliculaDto,
+  ): Promise<string> {
+    const pelicula = await this.findOne(id);
+    this.peliculaRepository.merge(pelicula, UpdatePeliculaDto);
+    this.peliculaRepository.save(pelicula);
+    return `El libro con el id ${id} ha sido modificado`;
   }
-
-  async remove(id: number): Promise<void> {
+  //eliminar pelicula
+  async remove(id: number): Promise<string> {
     await this.peliculaRepository.delete(id);
+    return `la pelicula con el id ${id} ha sido eliminado correctamente`;
   }
 }
